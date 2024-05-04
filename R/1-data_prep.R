dec06b45788376e0f49dbb63a8bb8b1e97371796
MArnasius <161752860+MArnasius@users.noreply.github.com>
2024-05-04 18:35:13 +0300

    1-data_prep.R

--- R/1-data_prep.R
+++ R/1-data_prep.R
@@ -1,6 +1,17 @@
 library(tidyverse)
+library(jsonlite)
 cat("Darbinė direktorija:", getwd())
-download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.csv.zip", "../data/temp")
+
+#------------------------------------------------------------------
+
+download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.json.zip", "../data/temp" )
 unzip("../data/temp",  exdir = "../data/")
+data = fromJSON('../data/monthly-2023.json')
 file.remove("../data/temp")
-file.remove("../data/monthly-2023.csv")
+file.remove("../data/monthly-2023.json")
+
+#------------------------------------------------------------------------
+
+data %>% 
+  filter(ecoActCode == 494100) %>%
+  saveRDS('../data/494100.rds')
