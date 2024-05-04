7791d5b295f24fde857224a758bc0637d42cefe3
MArnasius <161752860+MArnasius@users.noreply.github.com>
2024-05-04 18:36:20 +0300

    app.R

--- app/app.R
+++ app/app.R
@@ -1,1 +1,29 @@
 library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  titlePanel("Veiklos sritis: 494100"),
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput("kodas",
+                     "Pasirinkite įmonę iš sąrašo",
+                     choices = NULL),
+      
+    ),
+    mainPanel(
+      
+      plotOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  data <- readRDS("../data/494100.rds")
+  # padarymas, kad nerodytu imoniu, kuriu duomenu nera.
+  data=data %>% filter(is.na(avgWage)==FALSE)
+  
+  updateSelectizeInput(session, "kodas", choices = data$name , server = TRUE)
+  
+  
+  output$plot <- renderPlot(
+    data %>%
